b3edf701a58621dfd86da319763b68f8020a8b3f
Rudy1224 <luhuidi@vip.qq.com>
2015-03-21 10:36:55 +0800

    Simplified negative output control
    use `flag` as the coefficient to avoid determine statements

--- str_To_int.c
+++ str_To_int.c
@@ -3,12 +3,12 @@
 void str_To_int()
 {
 	char x[10];
-	int i = 0, num = 0, flag = 0;
+	int i = 0, num = 0, flag = 1;
 	printf("Input the number: ");
 	scanf_s("%s", &x, 9);
 	if (x[0] == '-')
 	{
-		flag = 1;
+		flag = -1;
 		i++;
 	}
 	while(x[i] != '\0')
@@ -16,10 +16,7 @@ void str_To_int()
 		num = num * 10 + (x[i] - '0');
 		i++;
 	}
-	if (flag == 1)
-		printf("The number is %d.\n", num*(-1));
-	else
-		printf("The number is %d.\n", num);
+	printf("The number is %d.\n", num*flag);
 }
 
 void main()
